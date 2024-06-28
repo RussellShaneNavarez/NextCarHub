64a8d0435ac453b1f5940222193e029de4d21ab6
Russell Shane Navarez <russellnavarez18@gmail.com>
2024-06-28 23:37:57 +0200

    EDIT: year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
                     className="object-contain"
                     />
                 <p className="text-base text-gray-700">
-                    CarHub 2023 <br />
+                    CarHub 2024 <br />
                     All rights reserved &copy;
                 </p>
             </div>
@@ -43,7 +43,7 @@ const Footer = () => {
             </div>
 
             <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-                <p>@2023 CarHub. All Rights Reserved</p>
+                <p>@2024 CarHub. All Rights Reserved</p>
                 <div
                 className="footer__copyrights-link">
                     <Link href="/"
